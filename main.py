7a5098f2989302570851b24d88668e6978e54168
TakshaPatel <112661354+TakshaPatel@users.noreply.github.com>
2025-03-19 09:09:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def speech_to_text():
 st.title("Your :green[Friendly] Better :blue[AI] Teacher:")
 st.text("By Taksha           (Please know I do not control the ads shown)")
 
-user_prompt = st.text_area("Write your prompt:", height=50)
+user_prompt = st.text_area("Prompt:", height=75)
 response_text = ""
 
 if st.button("🎙 Speak to it (might not work, idk)"):
